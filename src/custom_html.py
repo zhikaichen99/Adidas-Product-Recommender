e24080924484554fd8e1a327563903d9624180b9
zhikaichen99 <zhikaichen1999@gmail.com>
2023-08-02 14:34:17 -0400

    html

---
+++ src/custom_html.py
@@ -0,0 +1,14 @@
+# custom_html.py
+
+html_string = """
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="{url}" alt="Image" style="width:{image_width}px;height:auto;">
+    </div>
+    <div class="flip-card-back">
+      <p>{description}</p>
+    </div>
+  </div>
+</div>
+"""
