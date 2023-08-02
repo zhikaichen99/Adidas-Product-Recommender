9166f6e0438024e65191867a779e8b8159920f27
zhikaichen99 <zhikaichen1999@gmail.com>
2023-08-02 14:33:46 -0400

    css file

---
+++ src/custom_css.py
@@ -0,0 +1,42 @@
+# custom_css.py
+
+css_string = """
+<style>
+    .flip-card {
+      background-color: transparent;
+      width: 100%;
+      height: 100%;
+      perspective: 1000px;
+    }
+
+    .flip-card-inner {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      transition: transform 0.6s;
+      transform-style: preserve-3d;
+    }
+
+    .flip-card:hover .flip-card-inner {
+      transform: rotateY(180deg);
+    }
+
+    .flip-card-front, .flip-card-back {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      backface-visibility: hidden;
+    }
+
+    .flip-card-front {
+      background-color: #f1f1f1;
+    }
+
+    .flip-card-back {
+      background-color: #4CAF50;
+      color: white;
+      transform: rotateY(180deg);
+    }
+</style>
+"""
