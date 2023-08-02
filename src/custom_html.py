cabb5b8a4a0c478c42ec8418abc7b81585081be4
zhikaichen99 <zhikaichen1999@gmail.com>
2023-08-02 14:41:45 -0400

    fixed column issue

--- src/custom_html.py
+++ src/custom_html.py
@@ -1,13 +1,15 @@
 # custom_html.py
 
 html_string = """
-<div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="{url}" alt="Image" style="width:{image_width}px;height:auto;">
-    </div>
-    <div class="flip-card-back">
-      <p>{description}</p>
+<div class="image-container">
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img src="{url}" alt="Image" style="width:{image_width}px;height:auto;">
+      </div>
+      <div class="flip-card-back">
+        <p>{description}</p>
+      </div>
     </div>
   </div>
 </div>
