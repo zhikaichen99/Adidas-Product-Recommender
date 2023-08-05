24685064560d1116acca6db3f2ef3866d7082ff8
zhikaichen99 <zhikaichen1999@gmail.com>
2023-08-04 21:40:31 -0400

    embedded link to image

--- src/display_product_info.py
+++ src/display_product_info.py
@@ -1,28 +1,29 @@
 import streamlit as st
 
 def display_product_info(product_info_list):
+
     for i in range(0, len(product_info_list), 4):
         col1, col2, col3, col4 = st.columns(4)
 
-        col1.image(product_info_list[i]["image_url"], use_column_width=True, width=200)
+        col1.markdown(f'<a href="{product_info_list[i]["url"]}" target="_blank"><img src="{product_info_list[i]["image_url"]}" alt="Clickable Image" style="width:550px;"></a>', unsafe_allow_html=True)
         col1.subheader(product_info_list[i]["product"])
-        col1.text("Price: $" + product_info_list[i]["price"])
-        col1.text(product_info_list[i]["description"])
+        col1.write("Price: $" + product_info_list[i]["price"])
+        col1.write(product_info_list[i]["description"])
 
         if i+1 < len(product_info_list):
-            col2.image(product_info_list[i+1]["image_url"], use_column_width=True, width=200)
+            col2.markdown(f'<a href="{product_info_list[i+1]["url"]}" target="_blank"><img src="{product_info_list[i+1]["image_url"]}" alt="Clickable Image" style="width:550px;"></a>', unsafe_allow_html=True)
             col2.subheader(product_info_list[i+1]["product"])
-            col2.text("Price: $" + product_info_list[i+1]["price"])
-            col2.text(product_info_list[i+1]["description"])
+            col2.write("Price: $" + product_info_list[i+1]["price"])
+            col2.write(product_info_list[i+1]["description"])
 
         if i+2 < len(product_info_list):
-            col3.image(product_info_list[i+2]["image_url"], use_column_width=True, width=200)
+            col3.markdown(f'<a href="{product_info_list[i+2]["url"]}" target="_blank"><img src="{product_info_list[i+2]["image_url"]}" alt="Clickable Image" style="width:550px;"></a>', unsafe_allow_html=True)
             col3.subheader(product_info_list[i+2]["product"])
-            col3.text("Price: $" + product_info_list[i+2]["price"])
-            col3.text(product_info_list[i+2]["description"])
+            col3.write("Price: $" + product_info_list[i+2]["price"])
+            col3.write(product_info_list[i+2]["description"])
 
         if i+3 < len(product_info_list):
-            col4.image(product_info_list[i+3]["image_url"], use_column_width=True, width=200)
+            col4.markdown(f'<a href="{product_info_list[i+3]["url"]}" target="_blank"><img src="{product_info_list[i+3]["image_url"]}" alt="Clickable Image" style="width:550px;"></a>', unsafe_allow_html=True)
             col4.subheader(product_info_list[i+3]["product"])
-            col4.text("Price: $" + product_info_list[i+3]["price"])
-            col4.text(product_info_list[i+3]["description"])
+            col4.write("Price: $" + product_info_list[i+3]["price"])
+            col4.write(product_info_list[i+3]["description"])
