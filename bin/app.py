f4386226f2e2b17d8fbb9536b47bae71cc1c21c9
zhikaichen99 <zhikaichen1999@gmail.com>
2023-08-04 22:07:19 -0400

    changed file path of data

--- bin/app.py
+++ bin/app.py
@@ -8,7 +8,6 @@ parent_folder_path = os.path.abspath(os.path.join(current_path, os.pardir))
 sys.path.append(parent_folder_path)
 
 from src.qa_chain import create_qa_chain
-from src.column_dict import create_column_dict
 from src.display_product_info import display_product_info
 from src.create_product_info import create_info
 
@@ -33,14 +32,16 @@ def main():
 
     st.write('<div class="navbar"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png"></div>', unsafe_allow_html=True)
 
-    st.title("Product Display App")
     user_input = st.text_input("What can I help you look for?", "")
     qa_chain = create_qa_chain(vector_db)
     response = qa_chain.run(user_input)
-    st.write(response.split("\n")[0].split(".")[1])
+    if response:
+        st.write(response.split("\n")[0].split(".")[1])
 
-    product_info_list = create_info(response)
-    display_product_info(product_info_list)
+        product_info_list = create_info(response)
+        display_product_info(product_info_list)
+    else:
+        st.write("Could not find any matches. Try searching for something else")
 
 if __name__ == "__main__":
     main()
